1894a6c3eb9fa08b199f27c294aa14445ddafc9f
iamcaiorian <caio.rianbr@gmail.com>
2025-05-11 21:51:52 -0300

    fix: restore files

---
+++ db/migrate/20250511154627_create_todolists.rb
@@ -0,0 +1,11 @@
+class CreateTodolists < ActiveRecord::Migration[8.0]
+    def change
+      create_table :todolists do |t|
+        t.string :title
+        t.string :subtitle
+        t.string :color
+  
+        t.timestamps
+      end
+    end
+  end
---
+++ db/migrate/20250511154705_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[8.0]
+    def change
+      create_table :tasks do |t|
+        t.string :title
+        t.text :description
+        t.datetime :deadline
+        t.boolean :done
+        t.integer :priority
+        t.references :todolist, null: false, foreign_key: true
+  
+        t.timestamps
+      end
+    end
+  end
